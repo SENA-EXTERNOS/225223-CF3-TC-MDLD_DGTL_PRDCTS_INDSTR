cd5517be0656e912c108610bff232e7acc32c6e8
JPaolaMontilloG <paola.montillog@gmail.com>
2022-11-04 14:01:31 -0500

    Update _custom.sass y Tema2.vue
    + Se ajusta la posición del banner superior (H9).
    + Se aplica color a las tarjetas del slider (H8).

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -36,100 +36,101 @@
 
     p.mb-5 A continuación, se muestran algunos métodos de selección de propuestas:
 
-    SlyderA(tipo="b").mb-5
-      .row
-        .col-md-6.mb-4.mb-md-0
-          h4 
-            em Focus group 
-          p El grupo de enfoque de discusión o Focus group, es una técnica de tipo colaborativo en la que diferentes personas se encuentran a fin de resolver una problemática específica, es algo similar a lo que se conoce o se denomina como lluvia de ideas. Se utiliza en el campo laboral con diversos propósitos, que pueden ir, desde la selección del personal, hasta el lanzamiento de un nuevo producto de la empresa.
-          p Esta técnica sirve para aplicar en una gran cantidad de situaciones, de forma interna, por ejemplo, para conocer el punto de vista de los empleados acerca de un tema específico. También es útil para conocer la opinión de los consumidores acerca de alguno productos o servicios que se tengan como es nuestro caso. En conclusión, esta técnica puede ser de mucha utilidad cuando se necesita recoger la opinión de diferentes personas en un tema en particular, ya que esta información será de gran utilidad en el momento de tomar decisiones frente al producto a diseñar.
-        .col-md-6
-          figure
-            img(src='@/assets/curso/temas/tema2/img2-2.png', alt='Reunión de personas')
+    .tarjeta.tarjeta--azul.p-4.mb-5
+      SlyderA(tipo="b").mb-5
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h4 
+              em Focus group 
+            p El grupo de enfoque de discusión o Focus group, es una técnica de tipo colaborativo en la que diferentes personas se encuentran a fin de resolver una problemática específica, es algo similar a lo que se conoce o se denomina como lluvia de ideas. Se utiliza en el campo laboral con diversos propósitos, que pueden ir, desde la selección del personal, hasta el lanzamiento de un nuevo producto de la empresa.
+            p Esta técnica sirve para aplicar en una gran cantidad de situaciones, de forma interna, por ejemplo, para conocer el punto de vista de los empleados acerca de un tema específico. También es útil para conocer la opinión de los consumidores acerca de alguno productos o servicios que se tengan como es nuestro caso. En conclusión, esta técnica puede ser de mucha utilidad cuando se necesita recoger la opinión de diferentes personas en un tema en particular, ya que esta información será de gran utilidad en el momento de tomar decisiones frente al producto a diseñar.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema2/img2-2.png', alt='Reunión de personas')
 
-      .row
-        .col-md-6.mb-4.mb-md-0
-          p Para realizar esta técnica se reúnen de 6 a 10 personas aproximadamente, en un lugar que sea lo más tranquilo posible, los escenarios para ello pueden ser una sala de juntas o de conferencias, etc. En este espacio se debate inicialmente sobre un tema determinado, allí las personas dan su punto de vista, también proponen ideas, que son debatibles e interpeladas por sus pares o compañeros de trabajo. Para que esto fluya organizada y adecuadamente, se debe tener un moderador, por lo general o lo más apropiado, es que este tenga un cargo directivo dentro de la compañía; los expertos indican que es conveniente tener una persona externa para ello, quien cumpla este rol, tendrá la función de dirigir la reunión, moderar los momentos de las intervenciones y por supuesto escribir para dejar un registro, bajo el criterio de la imparcialidad.
-        .col-md-6
-          figure
-            img(src='@/assets/curso/temas/tema2/img2-3.png', alt='Reunión de personas')
+        .row
+          .col-md-6.mb-4.mb-md-0
+            p Para realizar esta técnica se reúnen de 6 a 10 personas aproximadamente, en un lugar que sea lo más tranquilo posible, los escenarios para ello pueden ser una sala de juntas o de conferencias, etc. En este espacio se debate inicialmente sobre un tema determinado, allí las personas dan su punto de vista, también proponen ideas, que son debatibles e interpeladas por sus pares o compañeros de trabajo. Para que esto fluya organizada y adecuadamente, se debe tener un moderador, por lo general o lo más apropiado, es que este tenga un cargo directivo dentro de la compañía; los expertos indican que es conveniente tener una persona externa para ello, quien cumpla este rol, tendrá la función de dirigir la reunión, moderar los momentos de las intervenciones y por supuesto escribir para dejar un registro, bajo el criterio de la imparcialidad.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema2/img2-3.png', alt='Reunión de personas')
 
-      .row
-        .col-md-6.mb-4.mb-md-0
-          h4 Ventajas y desventajas del 
-            em Focus group 
-          p Habiendo conocido lo que significa y las implicaciones que tiene en la selección de la propuesta se abordarán las ventajas y desventajas.
-          p
-            b Ventajas
-          | Permite conocer de una manera más profunda, las capacidades que tienen los miembros del  equipo de trabajo.
-          
-          ol
-            li.mb-3
-              i.fas.fa-square.cab.me-3
-              | Posibilita recoger ideas(nuevas) e información, para así mejorar la toma de decisiones.
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h4 Ventajas y desventajas del 
+              em Focus group 
+            p Habiendo conocido lo que significa y las implicaciones que tiene en la selección de la propuesta se abordarán las ventajas y desventajas.
+            p
+              b Ventajas
+            | Permite conocer de una manera más profunda, las capacidades que tienen los miembros del  equipo de trabajo.
+            
+            ol
+              li.mb-3
+                i.fas.fa-square.cab.me-3
+                | Posibilita recoger ideas(nuevas) e información, para así mejorar la toma de decisiones.
 
-            li.mb-3
-              i.fas.fa-square.cab.me-3
-              | Descubre enfoques y estrategias nuevas, respecto a un tema en especifico.
+              li.mb-3
+                i.fas.fa-square.cab.me-3
+                | Descubre enfoques y estrategias nuevas, respecto a un tema en especifico.
 
-            li.mb-3
-              i.fas.fa-square.cab.me-3
-              | Aumenta la eficacia (trabajo en equipo), reduciendo el tiempo gastado si se realiza de manera individual.
+              li.mb-3
+                i.fas.fa-square.cab.me-3
+                | Aumenta la eficacia (trabajo en equipo), reduciendo el tiempo gastado si se realiza de manera individual.
 
-        .col-md-6
-          figure
-            img(src='@/assets/curso/temas/tema2/img2-4.png', alt='Personas revisando diseños')
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema2/img2-4.png', alt='Personas revisando diseños')
 
-      .row
-        .col-md-6.mb-4.mb-md-0
-          h4 Desventajas
-          p Por otro lado, y en cuanto a las desventajas de los 
-            em focus group:
-                    
-          ol
-            li.mb-3
-              i.fas.fa-square.cab.me-3
-              | Es difícil realizar esta técnica a gran escala.
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h4 Desventajas
+            p Por otro lado, y en cuanto a las desventajas de los 
+              em focus group:
+                      
+            ol
+              li.mb-3
+                i.fas.fa-square.cab.me-3
+                | Es difícil realizar esta técnica a gran escala.
 
-            li.mb-3
-              i.fas.fa-square.cab.me-3
-              | Se obtienen resultados poco certeros al recibir retroalimentación que viene de manera subjetiva o poco sincera.
+              li.mb-3
+                i.fas.fa-square.cab.me-3
+                | Se obtienen resultados poco certeros al recibir retroalimentación que viene de manera subjetiva o poco sincera.
 
-            li.mb-3
-              i.fas.fa-square.cab.me-3
-              | Hay posibilidad de perder el control del grupo en cuestión o que los participantes pierdan el interés sobre el tema del que se trata.
+              li.mb-3
+                i.fas.fa-square.cab.me-3
+                | Hay posibilidad de perder el control del grupo en cuestión o que los participantes pierdan el interés sobre el tema del que se trata.
 
-            li.mb-3
-              i.fas.fa-square.cab.me-3
-              | Se necesita un moderador que sea empático, para sacar así lo mejor de cada participante.
+              li.mb-3
+                i.fas.fa-square.cab.me-3
+                | Se necesita un moderador que sea empático, para sacar así lo mejor de cada participante.
 
-        .col-md-6
-          figure
-            img(src='@/assets/curso/temas/tema2/img2-5.png', alt='Personas reunidas en un salón')
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema2/img2-5.png', alt='Personas reunidas en un salón')
 
-      .row
-        .col-md-6.mb-4.mb-md-0
-          h4 
-            b Matriz MVA
-          p La audiencia mínima viable representado en sus siglas como MVA (
-            em The Minimum Viable Audience
-            |), representa la audiencia más pequeña posible que puede sostener un producto, bien o servicio a medida que comienza un mercado pequeño. Una de las características distintivas del MVA es acercarse a los mercados ya existentes, para encontrar puntos en común donde haya personas (clientes) insatisfechas con los productos que se ofrecen, ya que no cumplen con sus iniciativas o gustos. Es decir, que esta audiencia mínima viable no intenta hacer algo para un grupo global, al contrario, lo que quiere es realizar o construir un producto, bien o servicio, que satisfaga una pequeña porción de la población objetiva; acudiendo a su insatisfacción no resuelta en el mercado comercial.
-          p Puntos claves para tener en cuenta:
-          p El aspecto principal del MVA (Audiencia Mínima Viable) es acercarse a los mercados ya existentes para encontrar a aquellas clientes, que no están satisfechas y así lograr construir algo de manera excepcional dando respuestas a sus demandas.
-        .col-md-6
-          figure
-            img(src='@/assets/curso/temas/tema2/img2-6.png', alt='Manos en portátil y celular')
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h4 
+              b Matriz MVA
+            p La audiencia mínima viable representado en sus siglas como MVA (
+              em The Minimum Viable Audience
+              |), representa la audiencia más pequeña posible que puede sostener un producto, bien o servicio a medida que comienza un mercado pequeño. Una de las características distintivas del MVA es acercarse a los mercados ya existentes, para encontrar puntos en común donde haya personas (clientes) insatisfechas con los productos que se ofrecen, ya que no cumplen con sus iniciativas o gustos. Es decir, que esta audiencia mínima viable no intenta hacer algo para un grupo global, al contrario, lo que quiere es realizar o construir un producto, bien o servicio, que satisfaga una pequeña porción de la población objetiva; acudiendo a su insatisfacción no resuelta en el mercado comercial.
+            p Puntos claves para tener en cuenta:
+            p El aspecto principal del MVA (Audiencia Mínima Viable) es acercarse a los mercados ya existentes para encontrar a aquellas clientes, que no están satisfechas y así lograr construir algo de manera excepcional dando respuestas a sus demandas.
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema2/img2-6.png', alt='Manos en portátil y celular')
 
-      .row
-        .col-md-6.mb-4.mb-md-0
-          h4 
-            b Entrevista con el cliente
-          p El entrevistar a los clientes es una parte importante en la creación de un producto industrial y/o una empresa, implica salir a hablar con las personas que se cree, pueden ser futuros clientes, esto es muy valioso y se recomienda hacerlo desde el primer momento en que se genera la idea del producto. Al realizar esta actividad de conversar o entrevistar a un cliente potencial, ya se está gestando de manera indirecta y fácil el estudio de mercado del producto. Además, con estas interacciones con el público, se da la posibilidad de generar conocimiento validado por su experiencia en el mercado.
-          p Las entrevistas a clientes son acciones necesarias, ya que constituyen una herramienta esencial que se debe conocer, dominar y aplicar de manera adecuada. Por último, siempre se debe de tener claro, cuál será la información que se quiere obtener, la formulación correcta de las preguntas y a qué personas van dirigidas.
-          
-        .col-md-6
-          figure
-            img(src='@/assets/curso/temas/tema2/img2-7.png', alt='Joven usando pantalla interactiva')
+        .row
+          .col-md-6.mb-4.mb-md-0
+            h4 
+              b Entrevista con el cliente
+            p El entrevistar a los clientes es una parte importante en la creación de un producto industrial y/o una empresa, implica salir a hablar con las personas que se cree, pueden ser futuros clientes, esto es muy valioso y se recomienda hacerlo desde el primer momento en que se genera la idea del producto. Al realizar esta actividad de conversar o entrevistar a un cliente potencial, ya se está gestando de manera indirecta y fácil el estudio de mercado del producto. Además, con estas interacciones con el público, se da la posibilidad de generar conocimiento validado por su experiencia en el mercado.
+            p Las entrevistas a clientes son acciones necesarias, ya que constituyen una herramienta esencial que se debe conocer, dominar y aplicar de manera adecuada. Por último, siempre se debe de tener claro, cuál será la información que se quiere obtener, la formulación correcta de las preguntas y a qué personas van dirigidas.
+            
+          .col-md-6
+            figure
+              img(src='@/assets/curso/temas/tema2/img2-7.png', alt='Joven usando pantalla interactiva')
     
     p Para aprender más acerca de la selección de propuesta y tener conocimientos más claros sobre el 
      b cliente 
