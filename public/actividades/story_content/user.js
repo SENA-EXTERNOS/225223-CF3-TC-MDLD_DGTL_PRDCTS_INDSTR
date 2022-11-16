1872c08f7d5637f4b22f47e195c95965ad47345b
JPaolaMontilloG <paola.montillog@gmail.com>
2022-11-16 07:18:09 -0500

    Update Actividad.vue
    - Se actualiza la AD.
    - Se cambia el ícono de la AD.

--- public/actividades/story_content/user.js
+++ public/actividades/story_content/user.js
@@ -2,7 +2,7 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6ZcUskliF30":
+      case "6DFlQqUD6P0":
         Script1();
         break;
   }
--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -7,7 +7,7 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/curso/ad.svg", alt="Mano señalando elementos")
+            img(src="@/assets/curso/arrastrar.svg", alt="Mano señalando elementos")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo.color-acento-botones
           h2 Identificar necesidades del cliente y ciclo de vida del producto
