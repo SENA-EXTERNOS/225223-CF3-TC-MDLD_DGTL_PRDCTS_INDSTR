3466c3d85cf18d9caac75b490a3147dba583f62f
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2023-02-12 20:44:26 -0500

    Se cambia videos de intro.

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -18,7 +18,7 @@
 
     figure.mb-5
     .video
-      iframe(width="560" height="315" src="https://www.youtube.com/embed/-kfawFg5wXA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      iframe(width="560" height="315" src="https://www.youtube.com/embed/5SgDHslfgMw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
 </template>
 
